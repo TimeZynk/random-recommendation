9d4b4da0e81e447337bb52f417b2ae200c6181e8
Chuck Chen <chuck@timezynk.com>
2020-11-19 12:26:41 +0100

    ids fetched, but not doing anything.

--- src/main.py
+++ src/main.py
@@ -11,24 +11,26 @@ TZBACKEND_URL = os.getenv('TZBACKEND_URL')
 app = Flask(__name__)
 api = Api(app)
 
-#base = "http://localhost:8989/"
-
 @app.route("/api/ml/v1/recommendation", methods=['GET'])
 def recommend_and_return():
     payload = ""
     number = int(request.args.get("limit"))
+
+    #shift_ids acquired, but not doing anything with it yet.
+    ids_qp = request.args.get("ids")
+    shift_ids = [id.strip() for id in ids_qp.split(',')]
+
     headers = {"Authorization":request.headers["Authorization"]}
-    #users = requests.request("GET", base + 'api/users', data=payload, headers=headers)
     users = requests.request("GET", TZBACKEND_URL + '/users', data=payload, headers=headers)
 
     data = json.loads(users.text)
 
-    ids = [person['id'] for person in data]
-    random.shuffle(ids)
-    res = ids[:number] if len(ids)>number else ids
+    user_ids = [person['id'] for person in data]
+    random.shuffle(user_ids)
+    res = user_ids[:number] if len(user_ids)>number else user_ids
     return jsonify(res)
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
