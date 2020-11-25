39048e35152008322990259feec99072bc62c595
Chuck Chen <chuck@timezynk.com>
2020-11-25 16:49:15 +0100

    exclude unavailable users

--- src/recommendation.py
+++ src/recommendation.py
@@ -13,13 +13,24 @@ def fetch_busy_users(qsse, url, headers):
         params = {'interval[start]' : qs[0], 'interval[end]': qs[1], 'interval[match]': "intersects"}
         response = requests.request("GET", url + '/shifts', headers=headers, params = params)
         overlapped_shifts = json.loads(response.text)
-        booked_users = []
+        booked_users = set()
         for shift in overlapped_shifts:
             if shift['booked']:
-                [booked_users.append(user) for user in shift['booked-users'] if user not in booked_users]
+                booked_users = booked_users.union(set(shift['booked-users']))  
         busy_users.append(booked_users)
 
     return busy_users
+
+def fetch_unavailable_users(qsse, url, headers):
+    unavailable_users = []
+    for qs in qsse:
+        params = {'interval[start]' : qs[0], 'interval[end]': qs[1], 'interval[match]': 'intersects', 'available': 'false'}
+        response = requests.request("GET", url + '/availabilities', headers=headers, params = params)
+        unavailabilities = json.loads(response.text)
+        unavailable_list = set(map(lambda x: x['user-id'], unavailabilities))
+        unavailable_users.append(unavailable_list)
+
+    return unavailable_users
 
 def fetch_shifts_start_end(shifts, url, headers):
     response = requests.request("GET", url + '/shifts', headers=headers)
@@ -43,10 +54,18 @@ def recommend_and_return():
 
     busy_users_list = fetch_busy_users(qsse, TZBACKEND_URL, headers)
 
+    unavailable_list = fetch_unavailable_users(qsse, TZBACKEND_URL, headers)
+
+    excluded_users_list = list(map(lambda x,y: x.union(y),busy_users_list, unavailable_list))
+
+    print(busy_users_list)
+    print(unavailable_list)
+    print(excluded_users_list)
+
     users = requests.request("GET", TZBACKEND_URL + '/users', headers=headers)
     users_data = json.loads(users.text)
     user_ids = [user['id'] for user in users_data]
-    avail_list = [[user for user in user_ids if user not in busy_users] for busy_users in busy_users_list]
+    avail_list = [[user for user in user_ids if user not in excluded_users] for excluded_users in excluded_users_list]
     res_list = []
     for users in avail_list:
         random.shuffle(users)
