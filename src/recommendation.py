5623d0d6eecb4350862b90da3d444026d971a964
Chuck Chen <chuck@timezynk.com>
2020-12-01 22:07:32 +0100

    work hours filtering added.

--- src/recommendation.py
+++ src/recommendation.py
@@ -1,5 +1,6 @@
 from flask import jsonify, request, Blueprint
 from datetime import datetime as dt
+from datetime import timedelta
 import requests
 import json
 import random
@@ -66,6 +67,85 @@ def fetch_ineligible_users(shifts, url, headers, user_id):
         
     return ineligible_users
 
+def fetch_work_hour_templates(url, headers, contracts):
+    fmt = "%Y-%m-%d"
+    users_dict = {}
+    for contract in contracts:
+        response_wh = requests.request("GET", url + '/work-hours-templates', headers=headers, params={"id": contract['template-id']})
+        wh = json.loads(response_wh.text)
+        template_dict = {
+                            'start-date': dt.strptime(contract['start-date'],fmt),
+                            'work-hours': wh[0]['rows'],
+                            'fulltime-hours': wh[0]['fulltime-hours']
+                        }
+        if 'end-date' in contract.keys():
+            template_dict['end-date'] = dt.strptime(contract['end-date'],fmt)
+
+        if contract['user-id'] in users_dict.keys():
+            users_dict[contract['user-id']].append(template_dict)
+        else:
+            users_dict[contract['user-id']] = [template_dict]
+    return users_dict
+
+def week_start_end(shift_time):
+    week_start = shift_time - timedelta(days = shift_time.weekday())
+    week_end = week_start + timedelta(days = 7)
+    week_start_real = dt(week_start.year, week_start.month, week_start.day)
+    week_end_real = dt(week_end.year, week_end.month, week_end.day) - timedelta(microseconds = 1)
+    return (week_start_real, week_end_real)
+
+
+def fulltime_hrs_and_work_hrs(qsse, work_hours_data, url, headers):
+    '''
+    In the current implementation we're under the assumption that the shifts are short and not 
+    exdended over several contracts.
+    '''
+    fmt = '%Y-%m-%dT%H:%M:%S.%f'
+    no_hrs_users = []
+    for qs in qsse:
+        no_hrs_set = set()
+        shift_start = dt.strptime(qs[0], fmt)
+        shift_end = dt.strptime(qs[1], fmt)
+        for user, work_hours_list in work_hours_data.items():
+            for wh in work_hours_list:
+                week_start, week_end = week_start_end(shift_start)
+                # not considering the possibility of shifts bring longer than 24hrs and could overlap with a contract by a late portion.
+                if shift_start >= wh['start-date'] and 'end-date' not in wh.keys():
+                    intersection = (max(week_start, wh['start-date']), week_end)
+                # Again not considering the situation of a shift last passing the end-date of a contract.
+                elif shift_start >= wh['start-date'] and shift_end <= wh['end-date']:
+                    intersection = (max(week_start, wh['start-date']), min(week_end, wh['end-date']))
+                else:
+                    continue
+                # what happens if user is included in a list of booked-users is not considered.
+                params = {
+                            "booked-users": user,
+                            "interval[start]": str(intersection[0]),
+                            "interval[end]": str(intersection[1]),
+                            "interval[match]": "intersects"
+                        }
+                response = requests.request("GET", url + '/shifts', headers=headers, params=params)
+                booked_shifts = json.loads(response.text)
+                time_diff = map(lambda x: min(dt.strptime(x['end'], fmt), week_end) - max(dt.strptime(x['start'], fmt), week_start), booked_shifts)
+                # sum of iter not supported.
+                accumulated = timedelta(0)
+                for x in time_diff:
+                    accumulated = accumulated + x
+                accumulated = accumulated + shift_end - shift_start
+                if (accumulated > timedelta(hours=wh['fulltime-hours'])):
+                    no_hrs_set.add(user)
+        no_hrs_users.append(no_hrs_set)
+    return no_hrs_users
+
+def fetch_no_work_hrs(qsse, url, headers):
+    response = requests.request("GET", url + '/employment-contracts', headers=headers, params={"archived": "null"})
+    contracts = json.loads(response.text)
+
+    work_hours_data = fetch_work_hour_templates(url, headers, contracts)
+
+    no_hrs_users = fulltime_hrs_and_work_hrs(qsse, work_hours_data, url, headers)
+    return no_hrs_users
+
 @recommendation.route("/api/ml/v1/recommendation", methods=['GET'])
 def recommend_and_return():
     number = int(request.args.get("limit"))
@@ -86,7 +166,9 @@ def recommend_and_return():
 
     ineligible_users_list = fetch_ineligible_users(query_shifts, TZBACKEND_URL, headers, user_id)
 
-    excluded_users_list = list(map(lambda x,y,z: x.union(y,z), busy_users_list, unavailable_list, ineligible_users_list))
+    no_work_hrs_list = fetch_no_work_hrs(qsse, TZBACKEND_URL, headers)
+
+    excluded_users_list = list(map(lambda x,y,z,a: x.union(y,z,a), busy_users_list, unavailable_list, ineligible_users_list, no_work_hrs_list))
 
     users = requests.request("GET", TZBACKEND_URL + '/users', headers=headers)
     users_data = json.loads(users.text)
