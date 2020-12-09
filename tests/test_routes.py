089bac6f8ebc2bd2ba555b1e1514bddb215a03bb
Chuck Chen <chuck@timezynk.com>
2020-12-09 19:07:42 +0100

    blacked test dir

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,8 +1,9 @@
-import pytest
-import sys
 from os import path
-sys.path.append(path.abspath(path.join(__file__, '../../src')))
+import sys
+
+sys.path.append(path.abspath(path.join(__file__, "../../src")))
 from recommendation import create_app
+import pytest
 
 
 @pytest.fixture
@@ -13,11 +14,10 @@ def app():
 
 
 def test_health_check(app):
-    url = '/api/health-check'
+    url = "/api/health-check"
     response = app.test_client().get(url)
     assert response.status_code == 200
 
+
 def test_recommend_and_return():
     pass
-
-
